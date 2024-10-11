e0d556343e0457c7240a41bc04bd2d5f1918f0db
Ebunoluwa Owoeye <ebunowoeye316@gmail.com>
2024-10-11 17:06:20 +0100

    background image changes

--- form-handler.php
+++ form-handler.php
@@ -1,10 +1,10 @@
-<?php
+<?php4744644
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-  $email_from = 'info@yourwebsite.com';
+  $email_from = 'info@yourdomain.com';
   $email_subject = 'New Form Submission';
 
   $email_body = "User Name: $name.\n".
