3e09a3b524d54d362daaae5b989d0c237468fd96
Ebunoluwa Owoeye <ebunowoeye316@gmail.com>
2024-11-13 21:12:34 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/send-email', (req, res) => {
+  const { email, message, name, subject } = req.body;
+
+  // Set up Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // or other email provider
+    auth: {
+      user: 'ebunowoeye316@gmail.com',
+      pass: 'lias pkaa omnf khql',
+    },
+  });
+
+  // Define mail options
+  const mailOptions = {
+    from: email,
+    to: 'ebunowoeye316@gmail.com',
+    subject: 'New Message from Contact Form',
+    text: `Name: ${name}\nEmail: ${email}\n\Subject: ${subject}\n\n${message}`,
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Error sending message');
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.send('Message sent successfully');
+    }
+  });
+});
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
